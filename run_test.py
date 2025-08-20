ec79495ca3fb1d8929a17bddad4879163dd6fa18
luigicabrera10 <luigi.cabrera.h@hotmail.com>
2025-08-20 11:35:19 -0500

    ✅ Some test were perform successfullly on Chatterbox component - Test results are included on this commit

--- run_test.py
+++ run_test.py
@@ -1,3 +1,5 @@
-from testing.test_openrouter.test import test_openrouter
+# from testing.test_openrouter.test import test_openrouter
+from testing.test_chatterbox.test import test_chatterbox
 
-test_openrouter()
+# test_openrouter()
+test_chatterbox()
