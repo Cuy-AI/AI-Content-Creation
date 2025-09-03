0cb6a220b8db5b202fae9b1631fd31b4cbd2efaf
luigicabrera10 <luigi.cabrera.h@hotmail.com>
2025-09-02 22:10:47 -0500

    ⚡️ Coded some test for LMStudio component - LMStudio component will not run inside a component class - Some OpenRouter component files and classes were updated - Volume path was changed (removed '_latest')

--- build.sh
+++ build.sh
@@ -4,9 +4,16 @@ set -e
 if [ -z "$1" ] || [ "$1" = "all" ]; then
   echo "Building all images..."
   docker build -t openrouter -f ./components/LM/OpenRouter/Dockerfile .
+  # docker build -t lmstudio -f ./components/LM/LMStudio/Dockerfile .       # This will not run on a Docker container
   # docker build -t chatterbox -f ./components/TTS/Chatterbox/Dockerfile .
   # docker build -t openvoice -f ./components/TTS/OpenVoice/Dockerfile .
 else
   echo "Building image for: $1"
-  docker build -t "$1-ai" ./components/$1
+  if [ "$1" = "openrouter" ]; then
+    docker build -t openrouter -f ./components/LM/OpenRouter/Dockerfile .
+  # elif [ "$1" = "lmstudio" ]; then
+  #   docker build -t lmstudio -f ./components/LM/LMStudio/Dockerfile .
+  else 
+    echo "Image $1 was not recognized"
+  fi
 fi
