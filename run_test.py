d1a94a3b1d8c77a66eb66f0f59e92e5277d5b400
luigicabrera10 <luigi.cabrera.h@hotmail.com>
2025-09-14 00:32:49 -0500

    ✨ New AI Whisper now runs as a container - Added a test with sample audio and video - Added new directory's into gitignore

--- run_test.py
+++ run_test.py
@@ -1,9 +1,11 @@
-from tests.test_openrouter.test import test_openrouter
-from tests.test_lmstudio.test import test_lmstudio
-from tests.test_chatterbox.test import test_chatterbox
-from tests.test_openvoice.test import test_openvoice
+# from tests.test_openrouter.test import test_openrouter
+# from tests.test_lmstudio.test import test_lmstudio
+# from tests.test_chatterbox.test import test_chatterbox
+# from tests.test_openvoice.test import test_openvoice
+from tests.test_whisper.test import test_whisper
 
-test_lmstudio()
-test_openrouter()
-test_chatterbox()
-test_openvoice()
+# test_lmstudio()
+# test_openrouter()
+# test_chatterbox()
+# test_openvoice()
+test_whisper()
